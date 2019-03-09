1abd2dc316ff1e173ce0896ed17214c337784e5a
Pavel Biriukov <syntheticforms@gmail.com>
2019-03-10 00:39:17 +0300

    added Koin
    * added Koin dependency
    
    * added Koin configuration

--- app/src/main/java/debts/DebtsApp.kt
+++ app/src/main/java/debts/DebtsApp.kt
@@ -2,7 +2,13 @@ package debts
 
 import android.app.Application
 import com.squareup.leakcanary.LeakCanary
+import debts.di.appModule
+import debts.di.repositoriesModule
+import debts.di.useCasesModule
 import net.thebix.debts.BuildConfig
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
 import timber.log.Timber
 
 class DebtsApp : Application() {
@@ -17,6 +23,21 @@ class DebtsApp : Application() {
         LeakCanary.install(this)
         if (BuildConfig.DEBUG) {
             Timber.plant(Timber.DebugTree())
+        }
+        // start Koin!
+        startKoin {
+            // use AndroidLogger as Koin Logger - default Level.INFO
+            androidLogger()
+
+            // use the Android context given there
+            androidContext(this@DebtsApp)
+
+            // module list
+            modules(
+                appModule,
+                repositoriesModule,
+                useCasesModule
+            )
         }
     }
 }
