cf3a77b3bfe9ce2ae1d5bae1c61c646879e1a957
Pavel Biriukov <syntheticforms@gmail.com>
2019-03-20 15:35:21 +0300

    implemented fade animation between fragments

--- app/src/main/java/debts/common/android/BaseActivity.kt
+++ app/src/main/java/debts/common/android/BaseActivity.kt
@@ -1,25 +1,60 @@
 package debts.common.android
 
 import androidx.annotation.IdRes
-import androidx.fragment.app.Fragment
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 
 abstract class BaseActivity : AppCompatActivity() {
 
-    fun replaceFragment(fragment: androidx.fragment.app.Fragment, @IdRes rootId: Int, addToBackStack: Boolean = true) {
+    fun replaceFragment(
+        fragment: Fragment, @IdRes rootId: Int,
+        addToBackStack: Boolean = true,
+        animations: List<Int> = listOf()
+    ) {
         val fragmentTransaction = supportFragmentManager
             .beginTransaction()
-            .replace(rootId, fragment)
+        if (animations.isNotEmpty()) {
+            when (animations.size) {
+                2 -> fragmentTransaction.setCustomAnimations(animations[0], animations[1])
+                4 -> fragmentTransaction.setCustomAnimations(
+                    animations[0],
+                    animations[1],
+                    animations[2],
+                    animations[3]
+                )
+                else -> {
+                    // no-op
+                }
+            }
+        }
+        fragmentTransaction.replace(rootId, fragment)
         if (addToBackStack) {
             fragmentTransaction.addToBackStack(null)
         }
         fragmentTransaction.commit()
     }
 
-    fun addFragment(fragment: androidx.fragment.app.Fragment, @IdRes rootId: Int, addToBackStack: Boolean = true) {
+    fun addFragment(
+        fragment: Fragment, @IdRes rootId: Int, addToBackStack: Boolean = true,
+        animations: List<Int> = listOf()
+    ) {
         val fragmentTransaction = supportFragmentManager
             .beginTransaction()
-            .add(rootId, fragment)
+        if (animations.isNotEmpty()) {
+            when (animations.size) {
+                2 -> fragmentTransaction.setCustomAnimations(animations[0], animations[1])
+                4 -> fragmentTransaction.setCustomAnimations(
+                    animations[0],
+                    animations[1],
+                    animations[2],
+                    animations[3]
+                )
+                else -> {
+                    // no-op
+                }
+            }
+        }
+        fragmentTransaction.add(rootId, fragment)
         if (addToBackStack) {
             fragmentTransaction.addToBackStack(null)
         }
