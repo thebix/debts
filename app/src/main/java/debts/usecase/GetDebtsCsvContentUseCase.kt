920c5070f4d96c884c0814dc14d2ba0c79a6b30c
Pavel Biriukov <syntheticforms@gmail.com>
2019-04-03 11:22:37 +0300

    fixed empty currency in csv file

--- app/src/main/java/debts/usecase/GetDebtsCsvContentUseCase.kt
+++ app/src/main/java/debts/usecase/GetDebtsCsvContentUseCase.kt
@@ -3,8 +3,8 @@ package debts.usecase
 import debts.common.android.extensions.toSimpleDateTimeString
 import debts.repository.DebtsRepository
 import io.reactivex.Single
-import io.reactivex.functions.BiFunction
-import java.util.*
+import io.reactivex.functions.Function3
+import java.util.Date
 
 class GetDebtsCsvContentUseCase(
     private val repository: DebtsRepository
@@ -14,13 +14,14 @@ class GetDebtsCsvContentUseCase(
         return Single.zip(
             repository.getDebtors(),
             repository.getDebts(),
-            BiFunction { debtors, debts ->
+            repository.getCurrency(),
+            Function3 { debtors, debts, currency ->
                 val debtorsMap = debtors.map { it.id to it }.toMap()
                 // Tech debt: pass translated strings from Fragment
-                val sb = StringBuilder("Date,\tName,\tAmount,\tComment")
+                val sb = StringBuilder("Date,\tName,\tAmount,\tCurrency\",\tComment")
                 debts.forEach { debt ->
                     debtorsMap[debt.debtorId]?.let { debtor ->
-                        sb.append("\n${Date(debt.date).toSimpleDateTimeString()},\t${debtor.name},\t${debt.amount},\t${debt.comment}")
+                        sb.append("\n${Date(debt.date).toSimpleDateTimeString()},\t${debtor.name},\t${debt.amount},\t$currency,\t${debt.comment}")
                     }
                 }
                 sb.toString()
