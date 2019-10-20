5c004102c84ffb320398b61706119e2947597880
Pavel Biriukov <pavel.biriukov@onefootball.com>
2019-10-20 18:24:19 +0200

    upgraded to LeakCanary 2.0-beta-3

--- app/src/main/java/debts/DebtsApp.kt
+++ app/src/main/java/debts/DebtsApp.kt
@@ -2,7 +2,6 @@ package debts
 
 import android.app.Application
 import com.crashlytics.android.Crashlytics
-import com.squareup.leakcanary.LeakCanary
 import debts.common.TimberCrashlyticsTree
 import debts.di.*
 import io.fabric.sdk.android.Fabric
@@ -16,12 +15,6 @@ class DebtsApp : Application() {
 
     override fun onCreate() {
         super.onCreate()
-        if (LeakCanary.isInAnalyzerProcess(this)) {
-            // This process is dedicated to LeakCanary for heap analysis.
-            // You should not init your app in this process.
-            return
-        }
-        LeakCanary.install(this)
         if (BuildConfig.DEBUG) {
             Timber.plant(Timber.DebugTree())
         } else {
