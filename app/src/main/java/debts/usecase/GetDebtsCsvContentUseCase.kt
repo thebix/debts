5a954e459a6a87a3748c30242eb61bc280a7c468
Pavel Biriukov <pavel.biriukov@onefootball.com>
2019-10-20 19:44:31 +0200

    fixed redundant quotes after Currency title in shared file

--- app/src/main/java/debts/usecase/GetDebtsCsvContentUseCase.kt
+++ app/src/main/java/debts/usecase/GetDebtsCsvContentUseCase.kt
@@ -18,7 +18,7 @@ class GetDebtsCsvContentUseCase(
             Function3 { debtors, debts, currency ->
                 val debtorsMap = debtors.map { it.id to it }.toMap()
                 // Tech debt: pass translated strings from Fragment
-                val sb = StringBuilder("Date,\tName,\tAmount,\tCurrency\",\tComment")
+                val sb = StringBuilder("Date,\tName,\tAmount,\tCurrency,\tComment")
                 debts.forEach { debt ->
                     debtorsMap[debt.debtorId]?.let { debtor ->
                         sb.append("\n${Date(debt.date).toSimpleDateTimeString()},\t${debtor.name},\t${debt.amount},\t$currency,\t${debt.comment}")
