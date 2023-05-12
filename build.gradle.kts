cb385acda3b646e32b0adddec271dab2dc38abd0
Pavel Biriukov <pavel@thebix.net>
2023-05-12 20:31:08 +0200

    migrated from Kapt to KSP

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@
 plugins {
     alias(libs.plugins.android.application) apply (false)
     alias(libs.plugins.kotlin.android) apply (false)
-    alias(libs.plugins.kotlin.kapt) apply (false)
+    alias(libs.plugins.ksp) apply (false)
     alias(libs.plugins.google.services) apply (false)
     alias(libs.plugins.triplet.play) apply (false)
     alias(libs.plugins.detekt)
